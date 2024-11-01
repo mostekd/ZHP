ae4e3521e955a53d56e86c68f8b0c1d1ebacf83b
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-11-01 19:15:48 +0100

    update

--- HSW/script.js
+++ HSW/script.js
@@ -1,15 +1,14 @@
-// Funkcja przełączająca widoczność sekcji
 function toggleSection(element) {
+    const isActive = element.classList.contains('active');
+
     // Wyłączanie aktywnych sekcji poza wybraną
     document.querySelectorAll('.card').forEach(card => {
-        if (card !== element) {
-            card.classList.remove('active');
-        }
+        card.classList.remove('active');
     });
 
-    // Animacja gładkiego przewijania do klikniętej karty
-    element.scrollIntoView({ behavior: 'smooth', block: 'center' });
-
-    // Przełączenie klasy active
-    element.classList.toggle('active');
+    // Przewijanie do sekcji, jeśli wcześniej nie była aktywna
+    if (!isActive) {
+        element.classList.add('active');
+        element.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
 }
